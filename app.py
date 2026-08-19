23f996a356076bbf00e4bd54df10019f3e07e43b
Shreyash-786 <pawarshreyash0000@gmail.com>
2026-08-19 15:30:31 +0530

    update in app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return '<p> hello world </p>'
+        return '<p> hello Shreyash </p>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
