bf33bb5610e61b7c2322b0b8ebb7dca940f014f4
Shreyash-786 <pawarshreyash0000@gmail.com>
2026-08-20 17:55:10 +0530

    upload file

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import time
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return '<p> hello Shreyash </p>'
+        return '<p> hello king</p>'
+@app.route('/home')
+def home():
+      for i in range(5):
+        return i
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
